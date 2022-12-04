74c4af2b82ae389a636e46506ad9bd4d18a81a1d
Oluwadamilare Olusakin <oluwadamilare@oluwadamilareolusakin.com>
2022-12-04 21:35:44 +0100

    Refactor and add cars endpoints

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -1,88 +1,143 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Router } from "express";
 // import bodyParser from 'body-parser'; deprecated
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-import { Car, cars as cars_list } from './cars';
+import { Car, cars as cars_list } from "./cars";
+import "./env";
 
-(async () => {
-  let cars:Car[]  = cars_list;
+// Redeclared these because there were some strange
+// TS errors that were present when importing the types from express
+interface Response {
+  status: (status: number) => Response;
+  send: (body: string) => Response;
+}
 
-  //Create an express application
-  const app = express(); 
-  //default port to listen
-  const port = 8082; 
-  
-  //use middleware so post bodies 
-  //are accessable as req.body.{{variable}}
-  app.use(bodyParser.json()); 
-  app.use(express.urlencoded({ extended: true })) //for requests from forms-like data
+interface Request {
+  params: { [key: string]: string };
+  query: { name: string; filters: { [key: string]: string } };
+  body: { [key: string]: string };
+}
 
-  // Root URI call
-  app.get( "/", ( req: Request, res: Response ) => {
-    res.status(200).send("Welcome to the Cloud!");
-  } );
+let cars: Car[] = cars_list;
 
-  // Get a greeting to a specific person 
-  // to demonstrate routing parameters
-  // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name", 
-    ( req: Request, res: Response ) => {
-      let { name } = req.params;
+//Create an express application
+const app = express();
+//default port to listen
+const port = process.env.SERVER_PORT;
 
-      if ( !name ) {
-        return res.status(400)
-                  .send(`name is required`);
-      }
+//use middleware so post bodies
+//are accessable as req.body.{{variable}}
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true })); //for requests from forms-like data
 
-      return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+// Root URI call
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Welcome to the Cloud!");
+});
 
-  // Get a greeting to a specific person to demonstrate req.query
-  // > try it {{host}}/persons?name=the_name
-  app.get( "/persons/", ( req: Request, res: Response ) => {
-    let { name } = req.query;
+// Get a greeting to a specific person
+// to demonstrate routing parameters
+// > try it {{host}}/persons/:the_name
+app.get("/persons/:name", (req: Request, res: Response) => {
+  let { name } = req.params;
 
-    if ( !name ) {
-      return res.status(400)
-                .send(`name is required`);
-    }
+  if (!name) {
+    return res.status(400).send(`name is required`);
+  }
 
-    return res.status(200)
-              .send(`Welcome to the Cloud, ${name}!`);
-  } );
+  return res.status(200).send(`Welcome to the Cloud, ${name}!`);
+});
 
-  // Post a greeting to a specific person
-  // to demonstrate req.body
-  // > try it by posting {"name": "the_name" } as 
-  // an application/json body to {{host}}/persons
-  app.post( "/persons", 
-    async ( req: Request, res: Response ) => {
+// Get a greeting to a specific person to demonstrate req.query
+// > try it {{host}}/persons?name=the_name
+app.get("/persons/", (req: Request, res: Response) => {
+  let { name } = req.query;
 
-      const { name } = req.body;
+  if (!name) {
+    return res.status(400).send(`name is required`);
+  }
 
-      if ( !name ) {
-        return res.status(400)
-                  .send(`name is required`);
-      }
+  return res.status(200).send(`Welcome to the Cloud, ${name}!`);
+});
 
-      return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+// Post a greeting to a specific person
+// to demonstrate req.body
+// > try it by posting {"name": "the_name" } as
+// an application/json body to {{host}}/persons
+app.post("/persons", async (req: Request, res: Response) => {
+  const { name } = req.body;
 
-  // @TODO Add an endpoint to GET a list of cars
-  // it should be filterable by make with a query paramater
+  if (!name) {
+    return res.status(400).send(`name is required`);
+  }
 
-  // @TODO Add an endpoint to get a specific car
-  // it should require id
-  // it should fail gracefully if no matching car is found
+  return res.status(200).send(`Welcome to the Cloud, ${name}!`);
+});
 
-  /// @TODO Add an endpoint to post a new car to our list
-  // it should require id, type, model, and cost
+// @TODO Add an endpoint to GET a list of cars
+// it should be filterable by make with a query paramater
+app.get("/cars", (req: Request, res: Response) => {
+  let response: Car | Car[] = cars;
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
-})();
+  if (req.query.filters) {
+    let { make } = req.query.filters;
+
+    response = cars.filter((car) => {
+      return car.make === make;
+    });
+  }
+
+  return res.status(200).send(JSON.stringify(response));
+});
+
+// @TODO Add an endpoint to get a specific car
+// it should require id
+// it should fail gracefully if no matching car is found
+app.get("/cars/:id", (req: Request, res: Response) => {
+  let { id } = req.params;
+
+  if (id) {
+    let car = cars.find((car) => car.id === parseInt(id));
+
+    if (car) return res.status(200).send(JSON.stringify(car));
+  }
+
+  return res.status(404).send("Searched high and low, couldn't find that car!");
+});
+
+/// @TODO Add an endpoint to post a new car to our list
+// it should require id, type, model, and cost
+app.post("/cars", (req: Request, res: Response) => {
+  let { body } = req;
+  let { id, type, model, cost, make } = body;
+
+  if (
+    !(
+      "id" in body &&
+      "type" in body &&
+      "model" in body &&
+      "make" in body &&
+      "cost" in body
+    )
+  ) {
+    return res.status(400).send("Missing required param!");
+  }
+
+  let car: Car = {
+    id: parseInt(id),
+    type,
+    model,
+    cost: parseInt(cost),
+    make,
+  };
+
+  cars.push(car);
+
+  return res.status(201).send(JSON.stringify(car));
+});
+
+// Start the Server
+export default app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`);
+  console.log(`press CTRL+C to stop server`);
+});
