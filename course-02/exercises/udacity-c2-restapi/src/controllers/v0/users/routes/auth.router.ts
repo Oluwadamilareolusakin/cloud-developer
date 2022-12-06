269bd63d3ddf95879a7ece7f6011af4c568d0885
Oluwadamilare Olusakin <oluwadamilare@oluwadamilareolusakin.com>
2022-12-06 22:46:58 +0100

    Rename env vars and stub auth

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -34,26 +34,27 @@ function generateJWT(user: User): string {
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-  if (!req.headers || !req.headers.authorization) {
-    return res.status(401).send({ message: "No authorization headers." });
-  }
+  return next()
+  // if (!req.headers || !req.headers.authorization) {
+  //   return res.status(401).send({ message: "No authorization headers." });
+  // }
 
-  const token_bearer = req.headers.authorization.split(" ");
+  // const token_bearer = req.headers.authorization.split(" ");
 
-  if (token_bearer.length != 2) {
-    return res.status(401).send({ message: "Malformed token." });
-  }
+  // if (token_bearer.length != 2) {
+  //   return res.status(401).send({ message: "Malformed token." });
+  // }
 
-  const token = token_bearer[1];
+  // const token = token_bearer[1];
 
-  return jwt.verify(token, config.jwt.secret, (err: any, decoded: any) => {
-    if (err) {
-      return res
-        .status(500)
-        .send({ auth: false, message: "Failed to authenticate." });
-    }
-    return next();
-  });
+  // return jwt.verify(token, config.jwt.secret, (err: any, decoded: any) => {
+  //   if (err) {
+  //     return res
+  //       .status(500)
+  //       .send({ auth: false, message: "Failed to authenticate." });
+  //   }
+  //   return next();
+  // });
 }
 
 router.get(
