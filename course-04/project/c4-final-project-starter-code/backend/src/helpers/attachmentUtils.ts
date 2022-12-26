624cf904476b40f1cf46dbb422499d88399c86b6
Oluwadamilare Olusakin <oluwadamilare@oluwadamilareolusakin.com>
2022-12-26 13:15:24 +0100

    [Final Project]: Write adapter for s3 signed url generation

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
@@ -1,16 +1,16 @@
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 
-const XAWS = AWSXRay.captureAWS(AWS);
-const S3Client = new XAWS.S3();
-const todoFilesBucket = process.env.ATTACHMENT_S3_BUCKET;
+const XAWS = AWSXRay.captureAWS(AWS)
+const S3Client = new XAWS.S3()
+const todoFilesBucket = process.env.ATTACHMENT_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 // TODO: Handle Errors
 
 export class AttachmentUtils {
-
-  generateUploadUrl(Key: string){
-    const Bucket = todoFilesBucket;
-    const Expires = 3600
+  generateUploadUrl(Key: string) {
+    const Bucket = todoFilesBucket
+    const Expires = urlExpiration
 
     const params = {
       Key,
@@ -18,10 +18,6 @@ export class AttachmentUtils {
       Expires
     }
 
-    return S3Client.getSignedUrl(
-      'putObject',
-      params
-    )
+    return S3Client.getSignedUrl('putObject', params)
   }
 }
-
