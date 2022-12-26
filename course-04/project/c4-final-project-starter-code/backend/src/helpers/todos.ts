95a6d1bc8dd918c76618a5e17fa4c56cb3b3f294
Oluwadamilare Olusakin <oluwadamilare@oluwadamilareolusakin.com>
2022-12-26 11:18:13 +0100

    [Final Project]: Implement port for Todos to interact with datalayer

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,10 +1,68 @@
 import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+import { AttachmentUtils } from './attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
 import * as createError from 'http-errors'
+import { timestamp } from './timestamp'
 
 // TODO: Implement businessLogic
+const logger = createLogger('Todos')
+
+export default class Todos {
+  TodosClient: TodosAccess
+
+  constructor() {
+    this.TodosClient = new TodosAccess()
+  }
+
+  createTodo(
+    userId: string,
+    attributes: CreateTodoRequest,
+    attachmentUrl?: string
+  ): TodoItem {
+    try {
+      const todoId = uuid()
+
+      const todoItem = {
+        todoId,
+        userId,
+        createdAt: timestamp(),
+        attachmentUrl,
+        done: false,
+        ...attributes
+      }
+
+      this.log(
+        `Creating todo for user ${userId}, attributes: ${{
+          ...attributes,
+          attachmentUrl
+        }}`
+      )
+      return this.TodosClient.createItem(todoItem)
+    } catch (e) {
+      this.log(`Failed to create todo`)
+
+      throw createError(e)
+    }
+  }
+
+  updateTodo(id: string, todo: UpdateTodoRequest): void {
+    try {
+      this.log(`Updating todo with id ${id}, attributes: ${todo}`)
+      this.TodosClient.updateItem(id, todo)
+    } catch (e) {
+      this.log(`Failed to update todo with id ${id}`)
+      throw createError(e)
+    }
+  }
+
+  log(message, level = 'info') {
+    logger.log({
+      level,
+      message
+    })
+  }
+}
