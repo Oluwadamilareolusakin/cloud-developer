afbbf6dfeb82cf78cab13fc2ba43d47cb0d98980
Oluwadamilare Olusakin <oluwadamilare@oluwadamilareolusakin.com>
2022-12-27 16:01:26 +0100

    [Final Project]: Remove expiry from presigned url generation. Explicitly define content type of object when generating presigned url

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
@@ -4,18 +4,15 @@ import * as AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
 const S3Client = new XAWS.S3()
 const todoFilesBucket = process.env.ATTACHMENT_S3_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
-// TODO: Handle Errors
 
-  export const generateUploadUrl = (Key: string) => {
-    const Bucket = todoFilesBucket
-    const Expires = urlExpiration
+export const generateUploadUrl = (Key: string, ContentType: string) => {
+  const Bucket = todoFilesBucket
 
-    const params = {
-      Key,
-      Bucket,
-      Expires
-    }
-
-    return S3Client.getSignedUrl('putObject', params)
+  const params = {
+    Key,
+    Bucket,
+    ContentType
   }
+
+  return S3Client.getSignedUrl('putObject', params)
+}
