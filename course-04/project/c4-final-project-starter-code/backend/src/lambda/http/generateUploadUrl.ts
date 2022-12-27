ce86a85c885179c4bacbdceeab367e533c53d3b1
Oluwadamilare Olusakin <oluwadamilare@oluwadamilareolusakin.com>
2022-12-27 23:04:21 +0100

    [Final Project]: Store download url for image on Todo when the url is generated

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -7,20 +7,38 @@ import cors from '@middy/http-cors'
 
 import { generateUploadUrl } from '../../helpers/attachmentUtils'
 import { getUserId } from '../utils'
+import Todos from '../../helpers/todos'
+
+const bucketUrl = process.env.ATTACHMENT_S3_BUCKET_URL
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoHelper = new Todos()
     const todoId = event.pathParameters.todoId
     const body = JSON.parse(event.body)
     const { filename, filetype } = body
     const userId = getUserId(event)
     const key = `${userId}/${todoId}/${filename}`
     const uploadUrl = generateUploadUrl(key, filetype)
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        uploadUrl
+
+    const attachmentUrl = `${bucketUrl}/${key}`
+
+    try {
+      await todoHelper.updateTodo(todoId, userId, {
+        attachmentUrl
       })
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          uploadUrl
+        })
+      }
+    } catch (e) {
+      return {
+        statusCode: 200,
+        body: `Problem generating your upload url ${e.message}`
+      }
     }
   }
 )
